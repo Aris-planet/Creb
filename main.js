fa83e0e44e428e8658460296a0f5846646ef39a8
Aris <112574414+Aris-planet@users.noreply.github.com>
2022-11-16 02:38:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+// filter
+let filterBtn = document.querySelectorAll('.filter-buttons .filter')
+let filterItem = document.querySelectorAll('.collections .box-card .collect')
+
+filterBtn.forEach(button => {
+
+    button.onclick = () => {
+        filterBtn.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        let dataFilter = button.getAttribute('data-filter');
+
+        filterItem.forEach(item => {
+
+            item.classList.remove('active');
+            item.classList.add('hide');
+
+            if (item.getAttribute('data-item') == dataFilter || dataFilter == 'all') {
+                item.classList.remove('hide');
+                item.classList.add('active');
+            }
+        })
+    }
+})
+
+var swiper = new Swiper(".card-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
+
+var swiper = new Swiper(".creator-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
